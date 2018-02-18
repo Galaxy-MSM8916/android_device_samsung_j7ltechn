6c50971431b4cfc04a2b3b88f647adceee5115a7
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-02-18 01:23:06 -0500

    j7ltechn: Add RR Makefile
    Change-Id: Ic3beae3ae75cf5146e6efd71760da0964b7e686a

---
+++ rr.mk
@@ -0,0 +1,14 @@
+# Inherit from common
+$(call inherit-product, device/samsung/j7lte-common/rr.mk)
+
+$(call inherit-product, device/samsung/j7ltechn/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := j7ltechn
+PRODUCT_NAME := rr_j7ltechn
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-J7008
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
