60567d5ee86cd0a59fa24e52e4d6b56422dc5a69
Emery Tang <emerytang@gmail.com>
2018-08-14 05:05:40 -0400

    j7ltespr: inherit from common before vendor
    Change-Id: I51efea1cc24803ef2da6430c6dcc888ac22efea6

--- device.mk
+++ device.mk
@@ -1,8 +1,8 @@
-# Inherit from vendor
-$(call inherit-product-if-exists, vendor/samsung/j7ltespr/j7ltespr-vendor.mk)
-
 # Inherit from common
 $(call inherit-product, device/samsung/j7lte-common/j7lte.mk)
+
+# Inherit from vendor
+$(call inherit-product-if-exists, vendor/samsung/j7ltespr/j7ltespr-vendor.mk)
 
 LOCAL_PATH := device/samsung/j7ltespr
 
