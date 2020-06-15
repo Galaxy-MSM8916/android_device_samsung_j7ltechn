c87877631f570f45cf89ece01aa604580d569caf
Dan Willemsen <dwillemsen@google.com>
2020-06-15 19:48:07 +0800

    j7ltechn: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Ib69479002e35113e0f81f4016cde5b561e74049a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,3 +4,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_j7ltechn.mk \
 	$(LOCAL_DIR)/pa_j7ltechn.mk \
 	$(LOCAL_DIR)/lineage_j7ltechn.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_j7ltechn-eng \
+    lineage_j7ltechn-userdebug \
+	lineage_j7ltechn-user
